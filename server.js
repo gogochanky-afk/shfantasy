dbe746b8387912eb7060197863624b8c7e31796b
gogochanky-afk <gogochanky@gmail.com>
2026-03-02 11:30:00 +0800

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-// server.js - Starts the backend server
-
-const app = require("./app");
-
-const PORT = process.env.PORT || 8080;
-
-app.listen(PORT, () => {
-  console.log(`SH Fantasy Backend running on port ${PORT}`);
-});
